870be8ac562dc9c3060f7279e32f87a29ccb4ad8
Juan Morales <jmoralespuello@gmail.com>
2024-09-11 08:43:44 -0500

    ADD: Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer class="footer mt-auto py-3 bg-body-tertiary">
+      <div class="container">
+        <span class="text-body-secondary"> 2024 | Juan Morales</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
